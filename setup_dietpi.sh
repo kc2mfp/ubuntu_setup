4f4642a500b70519927ece72dc545442861833fd
Carl <kc2mfp@aol.com>
2020-10-05 10:33:40 -0400

    added requirements for python

--- setup_dietpi.sh
+++ setup_dietpi.sh
@@ -9,19 +9,13 @@
 #apt-get install nmap
 
 #swapfile setup
-fallocate -l 3G /swapfile
-chmod 600 /swapfile
-sudo mkswap /swapfile
+#fallocate -l 3G /swapfile
+#chmod 600 /swapfile
+#sudo mkswap /swapfile
 # open file /etc/fstab
 # add line /swapfile swap swap defaults 0 0
 # Set swappiness to 60 /etc/sysctl.conf vm.swappiness=60
 
-
-#install Docker
-#apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
-#curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-#apt-key fingerprint 0EBFCD88
-#add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-#apt-get install docker-ce docker-ce-cli containerd.io
-
-
+#install python
+apt-get install python3.7
+apt-get install python3-pip
